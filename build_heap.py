a03d9b4216c58d2329b19323802d2c5d93d04e93
citizen2002 <90400687+citizen2002@users.noreply.github.com>
2023-06-01 15:36:36 +0300

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -2,66 +2,50 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
+    
     for i in range(len(data),0,-1):
-        currentElementIndex = i
-        canSwap = True
-        while canSwap:
-            if currentElementIndex < 2:
-                canSwap = False
+        currentElemIndex = i
+        swap = True
+        while swap:
+            if currentElemIndex < 2:
+                swap = False
                 break
-            parentElement = data[currentElementIndex//2-1]
-            currentElement = data[currentElementIndex-1]
+            parentElem = data[currentElemIndex//2-1]
+            currentElem = data[currentElemIndex-1]
 
-            if currentElement < parentElement:
-                data[currentElementIndex-1], data[currentElementIndex//2-1] = data[currentElementIndex//2-1],data[currentElementIndex-1]
-                swaps.append([currentElementIndex//2-1,currentElementIndex-1])
-                currentElementIndex = currentElementIndex//2
+            if currentElem < parentElem:
+                data[currentElemIndex-1], data[currentElemIndex//2-1] = data[currentElemIndex//2-1],data[currentElemIndex-1]
+                swaps.append([currentElemIndex//2-1,currentElemIndex-1])
+                currentElemIndex = currentElemtIndex//2
             else:
-                canSwap = False
+                swap = False
     return swaps
 
-
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-    # implement input form keyboard and from files
-    inpMethod = input()
-    # let user input file name to use, don't allow file names with letter a
+    impMethod = input()
     text = input("I or F: ")
     if "I" in text[:1]:
         n = int(input("number: "))
-        # input from keyboard
         data = list(map(int, input().split()))
     elif "F" in text[:1]:
-        filename = "test/" + input("Fails: ")
+        filename = "test/" + input("File: ")
         file = open(filename, "r")
         n = int(file.readline())
         data = file.readline()
         data = list(map(int, data.split()))
 
-
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
+    # ...
 
     # output all swaps
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    for a, b in swaps:
+        print(a, b)
 
 
 if __name__ == "__main__":
